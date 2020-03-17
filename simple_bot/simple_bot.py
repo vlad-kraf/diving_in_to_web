0f22eb2918b9b0b977bc13c0acbeecf6a1406c63
Vlad Kravchenko <vladkravchenkomail@gmail.com>
2020-03-17 22:23:14 +0000

    Classes of User and DB, Heroku preparation

--- simple_bot/simple_bot.py
+++ simple_bot/simple_bot.py
@@ -1,43 +1,11 @@
-from collections import defaultdict
 from telebot import TeleBot, types
-from config import token
+#from config import token
 import redis
 import os
 
+token = os.getenv('TOKEN')
 bot = TeleBot(token)
-"""
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
-r = redis.Redis(
-    host='localhost',
-    port=6379,
-    db=0,
-    decode_responses=True)
-#r = redis.from_url(redis_url, db=0, decode_responses=True)
-"""
-pool = redis.ConnectionPool(host='localhost', port=6379, db=0, decode_responses=True)
-r = redis.Redis(connection_pool=pool)
-
-
-def write_title_to_redis(message):
-    user_id = message.chat.id
-    location_title = message.text
-    r.lpush(user_id, location_title)
-
-
-def write_coords_to_redis(user_id, location):
-    lat, lon = location.latitude, location.longitude
-    title = r.lpop(user_id)
-    full_location_data = f'{title}&#124;{lat}&#124;{lon}'
-    r.lpush(user_id, full_location_data)
-
-
-def delete_location(user_id):
-    r.lpop(user_id)
-
-
-# User`s states.
-START, NEUTRAL, ADD_NAME, ADD_LOCATION, ADD_IMAGE, CONFIRMATION = range(6)
-USER_STATE = defaultdict(lambda: NEUTRAL)
+USERS = {}
 
 # texts for greetings and list of available commands.
 commands_list = """
@@ -49,21 +17,59 @@ There is a list of commands, that you can use:
 /hide_keyboard - hides additional keyboard.
 /show_keyboard - shows additional keyboard.\n
 """
-
 greeting_text = 'Howdy, how are you doing? \n'
-
 notice_text = 'Additional keyboard is shown to simplify input of commands.\n'
 
 
-# get current user`s state.
-def get_state(message):
-    return USER_STATE[message.chat.id]
+class BotError(Exception):
+    pass
 
 
-# update current user`s state.
-def update_state(message, state):
-    USER_STATE[message.chat.id] = state
+class RedisConnector:
+    """Class encapsulates connection to Redis and methods of interaction with db, that necessary to ChatBot"""
 
+    def __init__(self):
+        self.redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+        self.redis = redis.from_url(self.redis_url, db=0, decode_responses=True)
+
+    def add_place(self, message, title, location):
+        lat, lon = location.latitude, location.longitude
+        data = f'{title}&#124;{lat}&#124;{lon}'
+        self.redis.lpush(message.chat.id, data)
+
+    def remove_all_places(self, message):
+        self.redis.delete(message.chat.id)
+
+    def get_last_locations(self, message):
+        return self.redis.lrange(message.chat.id, 0, 10)
+
+
+red = RedisConnector()
+
+
+class User:
+    """Class that describes user object and it's methods"""
+
+    def __init__(self, message):
+        self.user_id = message.chat.id
+        self.state = 'NEUTRAL'
+        self.new_place = ''
+        self.new_location = {}
+        self.available_states = ('NEUTRAL', 'ADD_NAME', 'ADD_LOCATION', 'ADD_IMAGE', 'CONFIRMATION')
+
+    def get_state(self):
+        return self.state
+
+    def update_state(self, new_state):
+        if new_state in self.available_states:
+            self.state = new_state
+        else:
+            raise BotError("There is not such state")
+
+
+#####################################################################################################
+#                                           Block Keyboards                                         #
+#####################################################################################################
 
 # cancel bottom to interrupt operation.
 def cancel_button():
@@ -95,48 +101,56 @@ def additional_keyboard():
     keyboard.row(btn_help, btns_off)
     return keyboard
 
+#####################################################################################################
+#                                           End block Keyboards                                     #
+#####################################################################################################
 
-# checking if current command interrupt process of adding name of place, location or picture.
-def interrupt_check(message):
-    if get_state(message) in [ADD_NAME, ADD_LOCATION, ADD_IMAGE, CONFIRMATION]:
-        bot.send_message(message.chat.id, text='Process of adding a place was interrupted, please try again')
+
+#####################################################################################################
+#                                           Block TeleBot functions                                 #
+#####################################################################################################
+
+@bot.callback_query_handler(func=lambda x: True)
+def confirmation_handler(callback_query):
+    message = callback_query.message
+    data = callback_query.data
+    if data == 'cancel':
+        USERS[message.chat.id].new_location = {}
+        USERS[message.chat.id].new_place = ''
+        USERS[message.chat.id].update_state('NEUTRAL')
+
+        bot.edit_message_reply_markup(message.chat.id, message.message_id, reply_markup=None)
+        bot.send_message(message.chat.id, text="Process of adding a place was canceled.")
+    elif data == 'confirm':
+        USERS[message.chat.id].update_state('NEUTRAL')
+        red.add_place(message, USERS[message.chat.id].new_place, USERS[message.chat.id].new_location)
+        bot.edit_message_reply_markup(message.chat.id, message.message_id, reply_markup=None)
+        bot.send_message(message.chat.id, text="New Place was added.")
 
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    interrupt_check(message)
+def command_start(message):
+    # interrupt_check(message)
+    if message.chat.id not in USERS:
+        USERS[message.chat.id] = User(message)
 
-    update_state(message, NEUTRAL)
+    USERS[message.chat.id].update_state('NEUTRAL')
+
     keyboard = additional_keyboard()
     bot.send_message(message.chat.id, greeting_text+notice_text+commands_list, reply_markup=keyboard)
 
 
 @bot.message_handler(commands=['help'])
-def send_welcome(message):
-    interrupt_check(message)
+def command_help(message):
+    # interrupt_check(message)
 
-    update_state(message, NEUTRAL)
+    USERS[message.chat.id].update_state('NEUTRAL')
     bot.send_message(message.chat.id, commands_list)
 
 
-@bot.callback_query_handler(func=lambda x: True)
-def button_handler(callback_query):
-    message = callback_query.message
-    data = callback_query.data
-    if data == 'cancel':
-        update_state(message, NEUTRAL)
-        delete_location(message.chat.id)
-        bot.edit_message_reply_markup(message.chat.id, message.message_id, reply_markup=None)
-        bot.send_message(message.chat.id, text="Process of adding a place was canceled.")
-    elif data == 'confirm':
-        update_state(message, NEUTRAL)
-        bot.edit_message_reply_markup(message.chat.id, message.message_id, reply_markup=None)
-        bot.send_message(message.chat.id, text="New Place was added.")
-
-
 @bot.message_handler(commands=['show_keyboard'])
-def send_welcome(message):
-    interrupt_check(message)
+def command_show_keyboard(message):
+    # interrupt_check(message)
 
     keyboard = additional_keyboard()
     bot.send_message(
@@ -146,39 +160,38 @@ def send_welcome(message):
 
 
 @bot.message_handler(commands=['hide_keyboard'])
-def send_welcome(message):
-    interrupt_check(message)
+def command_hide_keyboard(message):
+    # interrupt_check(message)
 
     markup = types.ReplyKeyboardRemove()
-    chat_id = message.chat.id
-    bot.send_message(
-        chat_id,
-        "Keyboard hidden. You can turn it on in any time by command /show_keyboard.",
-        reply_markup=markup)
+    bot.send_message(message.chat.id,
+                     "Keyboard hidden. You can turn it on in any time by command /show_keyboard.",
+                     reply_markup=markup)
 
 
 # Chat commands:
 
 # Initiation of the adding new place.
 @bot.message_handler(commands=['add'])
-def echo_all(message):
-    interrupt_check(message)
+def command_add(message):
+    # interrupt_check(message)
 
-    update_state(message, ADD_NAME)
+    USERS[message.chat.id].update_state('ADD_NAME')
+
     cancel = cancel_button()
-    bot.send_message(message.chat.id, text='Please, type the name of new place or cancel the operation.',
+    bot.send_message(message.chat.id,
+                     text='Please, type the name of new place or cancel the operation.',
                      reply_markup=cancel)
 
 
 # Showing the list of recently added places.
 @bot.message_handler(commands=['list'])
-def echo_all(message):
-    interrupt_check(message)
+def command_list(message):
+    # interrupt_check(message)
 
-    update_state(message, NEUTRAL)
+    USERS[message.chat.id].update_state('NEUTRAL')
 
-    last_locations = r.lrange(message.chat.id, 0, 10)
-    print (last_locations)
+    last_locations = red.get_last_locations(message)
     if len(last_locations) == 0:
         bot.send_message(chat_id=message.chat.id, text='There are no locations')
     else:
@@ -195,39 +208,40 @@ def echo_all(message):
 
 # Removing all users data.
 @bot.message_handler(func=lambda x: True, commands=['reset'])
-def handle_confirmation(message):
-    r.flushdb()
+def command_reset(message):
+    red.remove_all_places(message)
     bot.send_message(chat_id=message.chat.id, text='All places is removed.')
 
 
 # Step 1: handling the name of the place.
-@bot.message_handler(func=lambda message: get_state(message) == ADD_NAME)
+@bot.message_handler(func=lambda message: USERS[message.chat.id].get_state() == 'ADD_NAME')
 def echo_all(message):
     if '/' not in message.text:
-        write_title_to_redis(message)
-        update_state(message, ADD_LOCATION)
+        USERS[message.chat.id].new_place = message.text
+        USERS[message.chat.id].update_state('ADD_LOCATION')
         cancel = cancel_button()
-        bot.send_message(chat_id=message.chat.id, text='Please, send your location or cancel the operation.',
+        bot.send_message(chat_id=message.chat.id,
+                         text='Please, send your location or cancel the operation.',
                          reply_markup=cancel)
     else:
         bot.send_message(chat_id=message.chat.id, text='Добавление прервано')
-        update_state(message, START)
+        USERS[message.chat.id].update_state('NEUTRAL')
 
 
 # Step 2: handling location.
-@bot.message_handler(func=lambda message: get_state(message) == ADD_LOCATION, content_types=['location'])
+@bot.message_handler(func=lambda message: USERS[message.chat.id].get_state() == 'ADD_LOCATION',
+                     content_types=['location'])
 def handle_location(message):
-    print("Location: ", message.location)
-    if message.text is None or '/' not in message.text: # Переделать проверку локации.
-        write_coords_to_redis(message.chat.id, message.location)
+    if message.text is None or '/' not in message.text:
+        USERS[message.chat.id].new_location = message.location
+        USERS[message.chat.id].update_state('CONFIRMATION')
 
-        update_state(message, CONFIRMATION)
         keyboard = comfirmation_button()
         bot.send_message(chat_id=message.chat.id, text='Do you want to add new location?',
                          reply_markup=keyboard)
     else:
         bot.send_message(chat_id=message.chat.id, text='Добавление прервано')
-        update_state(message, START)
+        USERS[message.chat.id].update_state('START')
 
 
 bot.polling()
